0dd228b3a8f9c7c3e3640da159a6745454f7d167
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-05-13 13:15:24 +0200

    Definición de métodos

--- src/main/scala/Conjunto/Conjunto.scala
+++ src/main/scala/Conjunto/Conjunto.scala
@@ -1,3 +1,14 @@
 package Conjunto
 
-class Conjunto[A] (val elementos: List[A])
+class Conjunto[A] (val elementos: List[A], LIMITE: BigInt) {
+  def apply(value: A): Boolean = ???
+  override def toString(): String = ???
+  def conjuntoUnElemento(value: A): Conjunto[A] = ???
+  def union(A: Conjunto[A], B: Conjunto[A]): Conjunto[A] = ???
+  def interseccion(A: Conjunto[A], B: Conjunto[A]): Conjunto[A] = ???
+  def diferencia(A: Conjunto[A], B: Conjunto[A]): Conjunto[A] = ???
+  def filtrar(value: A)(filtro: A => Boolean): Conjunto[A] = ???
+  def paraTodo(filtro: A => Boolean): Boolean = ???
+  def existe(valor: A)(filtro: A => Boolean): Boolean = ???
+  def map(transformacion: A => A): Conjunto[A] = ???
+}
