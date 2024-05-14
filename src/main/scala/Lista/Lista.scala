7c0bfd9f3d3b6180b53b8b5b66627c4f03e4486a
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-05-14 12:24:25 +0200

    Más métodos implementados

--- src/main/scala/Lista/Lista.scala
+++ src/main/scala/Lista/Lista.scala
@@ -69,7 +69,17 @@ def productoEnteros(enteros : Lista[Int]) : Double =
  * @param A
  * @return
  */
-def concatenar[A](lista1: Lista[A], lista2: Lista[A]): Lista[A] = ???
+def concatenar[A](lista1: Lista[A], lista2: Lista[A]): Lista[A] =
+  lista1 match
+    case Nil =>
+      lista2 match
+        case Nil => Nil
+        case Cons(_, _) => lista2
+
+    case Cons(cabezaLista1, colaLista1) =>
+      lista2 match
+        case Nil => lista1
+        case Cons(_, _) => Cons(cabezaLista1, concatenar(colaLista1, lista2))
 
 /**
  * Funcion de utilidad para aplicar una funcion de forma sucesiva a los
@@ -140,7 +150,17 @@ def tail[A](lista : Lista[A]): Lista[A] =
  * @param A tipo de datos
  * @return
  */
-def eliminar[A](lista : Lista[A], n: Int) : Lista[A] = ???
+def eliminar[A](lista : Lista[A], n: Int) : Lista[A] =
+
+  @annotation.tailrec
+  def go(nuevaLista: Lista[A], index: Int): Lista[A] =
+    if (index == n) nuevaLista
+    else {
+      nuevaLista match
+        case Nil => Nil
+        case Cons(_, cola) => go(cola, index + 1)
+    }
+  go(lista, 0)
 
 /**
  * Elimina elementos mientra se cumple la condicion pasada como
@@ -150,17 +170,24 @@ def eliminar[A](lista : Lista[A], n: Int) : Lista[A] = ???
  * @param A tipo de datos a usar
  * @return
  */
-def eliminarMientras[A](lista : Lista[A], criterio: A => Boolean) : Lista[A] = ???
+//def eliminarMientras[A](lista : Lista[A], criterio: A => Boolean) : Lista[A] = ???
+def eliminarMientras[A](lista: Lista[A], criterio: (A, A) => Boolean)(valor: A): Lista[A] =
+  lista match
+    case Nil => Nil
+    case Cons(cabeza, cola) => {
+      if (!criterio(cabeza, valor)) lista
+      else eliminarMientras(cola, criterio)(valor)
+    }
 
 
 /**
- 115 * Elimina el ultimo elemento de la lista. Aqui no se pueden compartir
- 116 * datos en los objetos y hay que generar una nueva lista copiando
- 117 * datos
- 118 * @param lista lista con la que trabajar
- 119 * @param A tipo de datos de la lista
- 120 * @return
- 121 */
+ * Elimina el ultimo elemento de la lista. Aqui no se pueden compartir
+ * datos en los objetos y hay que generar una nueva lista copiando
+ * datos
+ * @param lista lista con la que trabajar
+ * @param A tipo de datos de la lista
+ * @return
+ */
 def eliminarUltimo[A](lista : Lista[A]) : Lista[A] = ???
 
 /**
@@ -174,3 +201,58 @@ def eliminarUltimo[A](lista : Lista[A]) : Lista[A] = ???
  */
 //@annotation.tailrec
 def foldLeft[A, B](lista : Lista[A], neutro: B)(funcion : (B, A) => B): B = ???
+
+object criterios{
+  def mayorQue[A](x: A, y: A)(implicit ord: Ordering[A]): Boolean = ord.gt(x, y)
+  def mayorIgual[A](x: A, y: A)(implicit ord: Ordering[A]): Boolean = ord.gteq(x, y)
+}
+
+object prueba extends App{
+  val miLista = Lista(1, 2, 3, 4)
+  println("============")
+  println("Suma enteros")
+  println("============")
+  println(sumaEnteros(miLista))
+
+  println("============")
+  println("Producto enteros")
+  println("============")
+  println(productoEnteros(miLista))
+
+  println("============")
+  println("head sin modificar")
+  println("============")
+  println(head(miLista))
+
+  val nuevaLista = asignarCabeza(miLista, 2)
+
+  println("============")
+  println("head modificado")
+  println("============")
+  println(head(nuevaLista))
+
+  println("============")
+  println("longitud miLista")
+  println("============")
+  println(longitud(miLista))
+
+  println("============")
+  println("eliminar elementos")
+  println("============")
+  val miSegundaLista = eliminar(miLista, 4)
+  println(miSegundaLista)
+
+  println("============")
+  println("eliminar mientras")
+  println("============")
+  val listaDescendente = Lista(4, 3, 2, 1)
+  val miTerceraLista = eliminarMientras(listaDescendente, criterios.mayorIgual)(3)
+  println(miTerceraLista)
+
+  println("============")
+  println("concatenar")
+  println("============")
+  val A = Lista(1, 2, 3)
+  val B = Lista (4, 5)
+  println(concatenar(A, B))
+}
