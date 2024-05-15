ca92ae770c5e8f159e69e5f85f6a20007a990810
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-05-15 13:09:29 +0200

    Métodos para todo y existe implementados

--- src/main/scala/Conjunto/Conjunto.scala
+++ src/main/scala/Conjunto/Conjunto.scala
@@ -38,12 +38,6 @@ class Conjunto[A] (val elementos: List[A]) {
     val cadena = ""
     go(cadena, -LIMITE)
 
-  /**
-   * Creación de un conjunto dado por un unico elemento.
-   * @param value Valor con el que se crea el conjunto.
-   * @return
-   */
-  def conjuntoUnElemento(value: A): Conjunto[A] = new Conjunto(List(value))
 
   /**
    * Dados dos objetos de la clase Conjunto produce su unión.
@@ -82,10 +76,21 @@ class Conjunto[A] (val elementos: List[A]) {
    * del conjunto. Esta función debe implementarse de forma recursiva, definiendo una
    * función auxiliar, ya que hay que iterar sobre el rango de valores dado por LIMITE.
    * @param filtro Filtro aplicado al conjunto.
-   * @param value Valor al cual se compara
+   * @param valor Valor al cual se compara
    * @return Flag booleano que indica si se cumple la condición para todo elemento (true) o no (false).
    */
-  def paraTodo(filtro: (A, A) => Boolean)(value: A, LIMITE: Int): Boolean = ???
+  def paraTodo(filtro: (A, A) => Boolean)(valor: A, LIMITE: Int): Boolean =
+    def go(actual: Int): Boolean =
+      if (actual < LIMITE) {
+        if (apply(actual.asInstanceOf[A])) {
+          if (filtro(actual.asInstanceOf[A], valor)) go(actual + 1)
+          else false
+        }
+        else go(actual + 1)
+      }
+      else true
+
+    go (-LIMITE)
 
   /**
    * Determina si un conjunto contiene al menos un elemento para el que se cumple
@@ -94,37 +99,56 @@ class Conjunto[A] (val elementos: List[A]) {
    * @param filtro Filtro aplicado.
    * @return Flag booleano que indica si existe un valor tal que se cumpla el predicado (true) o no (false).
    */
-  def existe(valor: A)(filtro: A => Boolean): Boolean = ???
+  def existe(filtro: (A, A) => Boolean)(valor: A, LIMITE: Int): Boolean =
+    def go(actual: Int): Boolean =
+      if (actual < LIMITE) {
+        if (apply(actual.asInstanceOf[A])) {
+          if (filtro(actual.asInstanceOf[A], valor)) true
+          else go(actual + 1)
+        }
+        else go(actual + 1)
+      }
+      else false
+
+    go(-LIMITE)
 
   /**
    * Transforma un conjunto en otro aplicando una cierta función.
-   * @param mult Factor a aplicar.
+   * @param factor Factor a aplicar.
    * @return Conjunto transformado.
    */
-  def map(operacion: (A, A) => A)(mult: A): Conjunto[A] =
-    val nuevosElementos = elementos.map(e => operacion(e, mult))
+  def map(operacion: (A, A) => A)(factor: A): Conjunto[A] =
+    val nuevosElementos = elementos.map(e => operacion(e, factor))
 
     new Conjunto(nuevosElementos)
 }
+
+/**
+ * Creación de un conjunto dado por un unico elemento.
+ *
+ * @param value Valor con el que se crea el conjunto.
+ * @return
+ */
+def conjuntoUnElemento[A](value: A): Conjunto[A] = new Conjunto(List(value))
 
 object filtros {
   def esMayorQue[A](x: A, y: A)(implicit ord: Ordering[A]) = ord.gt(x, y)
 }
 
 object transformaciones {
-  def multiplica[A](x: A, multiplicador: A)(implicit numeric: Numeric[A]): A = numeric.times(x, multiplicador)
+  def multiplicaPor[A](x: A, multiplicador: A)(implicit numeric: Numeric[A]): A = numeric.times(x, multiplicador)
 }
 
 object prueba extends App{
   val conjunto1 = new Conjunto(List(-5, 1, 2, 3, 5, 6))
   val conjunto2 = new Conjunto(List(3, 4, 5, 6))
+  val unico = conjuntoUnElemento(3)
+  println("Conjunto1 => " + conjunto1.toString(10))
 
-  println("Conjunto1")
-  println("------------")
-  println(conjunto1.toString(10))
-  println("Conjunto2")
-  println("------------")
-  println(conjunto2.toString(10))
+  println("Conjunto2 => " + conjunto2.toString(10))
+
+  println("Conjunto con único elemento => " + unico.toString(10))
+
   val union = conjunto1.union(conjunto2)
 
   val interseccion = conjunto1.interseccion(conjunto2)
@@ -133,31 +157,23 @@ object prueba extends App{
 
   val filtrado = conjunto1.filtrar(filtros.esMayorQue)(3)
 
-  val paratodo = conjunto1.paraTodo(filtros.esMayorQue)(3, 10)
+  val paratodo = conjunto1.paraTodo(filtros.esMayorQue)(-10, 10)
 
-  //val existencia = conjunto1.existe()
+  val existencia = conjunto1.existe(filtros.esMayorQue)(8, 10)
 
-  val transformado = conjunto1.map(transformaciones.multiplica)(3)
+  val transformado = conjunto1.map(transformaciones.multiplicaPor)(3)
 
-  println("Unión")
-  println("------------")
-  println(union.toString(10))
-  println("Intersección")
-  println("------------")
-  println(interseccion.toString(10))
-  println("Diferencia")
-  println("------------")
-  println(diferencia.toString(10))
-  println("Filtrado")
-  println("------------")
-  println(filtrado.toString(10))
-  println("Para todo")
-  println("------------")
-  println(paratodo.toString)
-  println("Existencia")
-  println("------------")
-//  println(existencia.toString(10))
-  println("Transformado")
-  println("------------")
-  println(transformado.toString(15))
+  println("Unión (c1, c2) => " + union.toString(10))
+
+  println("Intersección (c1, c2) => " + interseccion.toString(10))
+
+  println("Diferencia (c1, c2) => " + diferencia.toString(10))
+
+  println("Filtrado (c1) => " + filtrado.toString(10))
+
+  println("Para todo (c1 > -10) => " + paratodo.toString)
+
+  println("Existe (c1, > 8) => " + existencia.toString)
+
+  println("Transformado (c1 * 3) => " + transformado.toString(15))
 }
