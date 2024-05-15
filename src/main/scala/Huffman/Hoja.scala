496140214517468436993b104657ad1dc8ec59aa
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-05-16 00:35:02 +0200

    Métodos apply , obtenerCaracteres y calcularPes

--- src/main/scala/Huffman/Hoja.scala
+++ src/main/scala/Huffman/Hoja.scala
@@ -1,8 +1,12 @@
 package Huffman
 
-class Hoja extends Nodo{
+class Hoja (caracter:Char, valor: Int) extends Nodo{
 
-  override def calcularPeso: BigInt = ???
+  override def calcularPeso: Int = valor
 
-  override def obtenerCaracteres: List[Char] = ???
+  override def obtenerCaracteres: List[Char] = List(caracter)
+
+  override def generaArbol: List[Nodo] = ???
 }
+
+def apply(caracter: Char, valor: Int): Hoja = new Hoja (caracter, valor)
--- src/main/scala/Huffman/Interno.scala
+++ src/main/scala/Huffman/Interno.scala
@@ -1,7 +1,27 @@
 package Huffman
 
-class Interno extends Nodo{
-  override def calcularPeso: BigInt = ???
+class Interno(var hijoI: Nodo, var hijoD: Nodo) extends Nodo{
+  override def calcularPeso:Int =
+    var pesoI = 0
+    var pesoD = 0
+    if (hijoI != null) pesoI = hijoI.calcularPeso
 
-  override def obtenerCaracteres: List[Char] = ???
+    if (hijoD != null) pesoD = hijoD.calcularPeso
+
+    pesoI + pesoD
+
+  override def obtenerCaracteres: List[Char] =
+
+    var caracteresI = List[Char]()
+    var caracteresD = List[Char]()
+
+    if (hijoI != null) caracteresI = hijoI.obtenerCaracteres
+    if (hijoD != null) caracteresD = hijoD.obtenerCaracteres
+
+    caracteresI ++ caracteresD
+
+  override def generaArbol: List[Nodo] = ???
+
 }
+
+def apply(hijoI: Nodo, hijoD: Nodo): Interno = new Interno(hijoI, hijoD)
