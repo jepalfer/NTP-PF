fe0d4e5eb1b42344b3b2f97f2f5874f550a10249
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-05-16 00:38:30 +0200

    Creación del tipo tabla de codificación

---
+++ src/main/scala/Huffman/TablaCodificacion.scala
@@ -0,0 +1,7 @@
+package Huffman
+
+type TablaCodificacion=Map[Char, String]
+
+def codificarConTabla(tabla : TablaCodificacion)(caracter : Char) : String = ???
+
+def convertirArbolTabla(arbolCodificacion : Nodo) : TablaCodificacion = ???
